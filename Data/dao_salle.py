84e9f8ae2dea1cf563343d06fa53c9abe976a194
LE BON COIN <saidguerda04@gmail.com>
2026-04-19 02:25:43 -0400

    j'ai créer la méthode qui permet de insérer un salle

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -1,18 +1,29 @@
 import mysql.connector
 import json
-from models import salle
+from models.salle import Salle
+
+class DataSalle:
+
+    def get_connection(self):
+        with open("data/config.json", "r") as fichier:
+            config = json.load(fichier)
+
+        connection = mysql.connector.connect(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+        return connection
+
+    def insert_salle(self, salle):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO salle VALUES (%s,%s,%s,%s)",
+                       (salle.code, salle.libelle, salle.type, salle.capacite))
+        conn.commit()
+        conn.close()
 
 
-def get_connection(self):
-    with open("data/config.json", "r") as fichier:
-        config = json.load(fichier)
-
-    connection = mysql.connector.connect(
-        host=config["host"],
-        user=config["user"],
-        password=config["password"],
-        database=config["database"]
-    )
-    return connection
 
 
