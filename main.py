3863ff5213c94fd79e3eedffe9d800e7e5aa26c7
LE BON COIN <saidguerda04@gmail.com>
2026-04-19 03:04:52 -0400

    je viens de isérer 3 salles

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from Data.dao_salle import DataSalle
 from models.salle import Salle
 
-dao = DataSalle()
+dat = DataSalle()
 
 try:
-    conn = dao.get_connection()
+    conn = dat.get_connection()
     print("Connexion réussie")
     conn.close()
 except Exception as e:
     print("Erreur connexion :", e)
+s1 = Salle("S1", "Salle Info", "Laboratoire", 30)
+s2 = Salle("S2", "Salle Réunion", "Bureau", 20)
+s3 = Salle("S3", "Salle Classe", "Classe", 40)
+
+dat.insert_salle(s1)
+dat.insert_salle(s2)
+dat.insert_salle(s3)
