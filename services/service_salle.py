684f3b9058be4e49d14264bc35e43537c043187f
LE BON COIN <saidguerda04@gmail.com>
2026-04-19 03:42:52 -0400

    j'ai créer la méthode ajouter salle dans services qui permet de ajouter un salle apres la verifications des données

---
+++ services/service_salle.py
@@ -0,0 +1,19 @@
+from Data.dao_salle import DataSalle
+
+class ServiceSalle:
+
+    def __init__(self):
+        self.dao = DataSalle()
+
+    def ajouter_salle(self, s):
+        if s.code == "" or s.libelle == "" or s.type == "" or s.capacite == "":
+            print("Champs vides")
+            return False
+        else:
+            if int(s.capacite) < 1:
+                print("Capacité invalide")
+                return False
+            else:
+                self.dao.insert_salle(s)
+                print("Salle ajoutée")
+                return True
