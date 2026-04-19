6998b6524082dc6646094738c6f1a3f4831a0900
LE BON COIN <saidguerda04@gmail.com>
2026-04-19 02:03:21 -0400

    j'ai créer la méthode afficher_infos qui permet de afficher les informations de la salle

--- models/salle.py
+++ models/salle.py
@@ -4,3 +4,6 @@ class Salle:
         self.libelle = libelle
         self.type = type
         self.capacite = capacite
+
+    def afficher_infos(self):
+        print(self.code, self.libelle, self.type, self.capacite)
