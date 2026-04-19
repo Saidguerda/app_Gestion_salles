ae0f8e7a0b9a15e2efc5c11ebd1fe0134ac5c148
LE BON COIN <saidguerda04@gmail.com>
2026-04-19 03:11:16 -0400

    je viens de modifier une classe

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ try:
     conn.close()
 except Exception as e:
     print("Erreur connexion :", e)
-s1 = Salle("S1", "Salle Info", "Laboratoire", 30)
-s2 = Salle("S2", "Salle Réunion", "Bureau", 20)
-s3 = Salle("S3", "Salle Classe", "Classe", 40)
+s1 = Salle("S5", "Salle Info", "Laboratoire", 30)
+s2 = Salle("S4", "Salle Réunion", "Bureau", 20)
+s3 = Salle("S0", "Salle Classe", "Classe", 40)
 
 dat.insert_salle(s1)
 dat.insert_salle(s2)
 dat.insert_salle(s3)
+
+s1_modif = Salle("S5", "Salle Informatique", "Lab", 35)
+dat.update_salle(s1_modif)
