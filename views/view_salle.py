9a5967157866df0042e43980fe28fc93f4f4d4a7
LE BON COIN <saidguerda04@gmail.com>
2026-04-20 00:04:33 -0400

    je viens de  ajouert service et configuration fenetre

--- views/view_salle.py
+++ views/view_salle.py
@@ -5,3 +5,8 @@ from models.salle import Salle
 class ViewSalle(ctk.CTk):
     def __init__(self):
         super().__init__()
+
+        self.service_salle = ServiceSalle()
+        self.title("Gestion des salles")
+        self.geometry("600x500")
+
