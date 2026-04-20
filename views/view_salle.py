0a5a49b4543ac912bcebcdd56e101308829804dc
LE BON COIN <saidguerda04@gmail.com>
2026-04-20 00:33:29 -0400

    je viens de créer les méthode pour chaque commande

--- views/view_salle.py
+++ views/view_salle.py
@@ -32,17 +32,40 @@ class ViewSalle(ctk.CTk):
         self.cadreActions = ctk.CTkFrame(self)
         self.cadreActions.pack(pady=10)
 
-        self.btnAjouter = ctk.CTkButton(self.cadreActions, text="Ajouter")
+        self.btnAjouter = ctk.CTkButton(self.cadreActions, text="Ajouter",command=self.ajouter_salle)
         self.btnAjouter.grid(row=0, column=0)
 
-        self.btnModifier = ctk.CTkButton(self.cadreActions, text="Modifier")
+        self.btnModifier = ctk.CTkButton(self.cadreActions, text="Modifier",command=self.modifier_salle)
         self.btnModifier.grid(row=0, column=1)
 
-        self.btnSupprimer = ctk.CTkButton(self.cadreActions, text="Supprimer")
+        self.btnSupprimer = ctk.CTkButton(self.cadreActions, text="Supprimer",command=self.supprimer_salle)
         self.btnSupprimer.grid(row=0, column=2)
 
-        self.btnRechercher = ctk.CTkButton(self.cadreActions, text="Rechercher")
+        self.btnRechercher = ctk.CTkButton(self.cadreActions, text="Rechercher",command=self.rechercher_salle)
         self.btnRechercher.grid(row=0, column=3)
+
+    def ajouter_salle(self):
+            s = Salle(self.code.get(), self.libelle.get(), self.type.get(), int(self.capacite.get()))
+            self.service_salle.ajouter_salle(s)
+
+    def modifier_salle(self):
+            s = Salle(self.code.get(), self.libelle.get(), self.type.get(), int(self.capacite.get()))
+            self.service_salle.modifier_salle(s)
+
+    def supprimer_salle(self):
+            self.service_salle.supprimer_salle(self.code.get())
+
+    def rechercher_salle(self):
+        s = self.service_salle.rechercher_salle(self.code.get())
+        if s:
+            self.libelle.delete(0, "end")
+            self.libelle.insert(0, s.libelle)
+
+            self.type.delete(0, "end")
+            self.type.insert(0, s.type)
+
+            self.capacite.delete(0, "end")
+            self.capacite.insert(0, s.capacite)
 
 
 
